5e52c93036b39b0453c5620a4c9ce38ac098041b
Ov3rrlord <desouzajulio16@gmail.com>
2023-07-30 16:59:56 +0100

    Added exception handling

--- bulk_mail.py
+++ bulk_mail.py
@@ -3,6 +3,7 @@
 from smtplib import SMTP_SSL
 from smtplib import SMTPAuthenticationError
 import ssl
+import sys
 
 # import maskpass as mk
 
@@ -12,77 +13,79 @@ mails = ""
 
 
 def main():
-    smtp_server = "smtp.gmail.com"
-    port = 465
-    print("***********************************")
-    print("* Welcome to the bulk mail sender *")
-    print("***********************************")
-    sender = "oversecdev@gmail.com"  # insert your mail here
-    subject = input("Please enter the subject:")
+    try:
+        smtp_server = "smtp.gmail.com"
+        port = 465
+        print("***********************************")
+        print("* Welcome to the bulk mail sender *")
+        print("***********************************")
+        sender = "oversecdev@gmail.com"  # insert your mail here
+        subject = input("Please enter the subject:")
 
-    def pwd():
-        global password
-        # you need to provide a file that contain your password. 
-        # The file needs to be in th same directory. Or you must specify its path
-        try:
-            with open("pass.txt", encoding="utf-8") as f:
-                for line in f:
-                    password = line.strip()
-            return password
-        except FileNotFoundError:
-            print("You need to provide a file containing your google account app password as pass.txt")
-
-    def bulk():
-        global receiver
-        # here you need to provide the file containing the mails you want to send messages to.
-        try:
-            with open("mails.txt", encoding="utf-8") as f:
-                lines = f.readlines()
-                for line in lines:
-                    receiver.append(line.strip())
-                return receiver
-        except FileNotFoundError:
-            print("You need to provide a file containing a list of mails as mails.txt")
-
-    def rest():
-        global password, mails  # new change
-        # receiver = input("Print the receiver email here: ")
-        content = input("Enter your message here: ")
-        context = ssl.create_default_context()
-        mails = bulk()
-
-        # connect to the smtp server
-        with SMTP_SSL(smtp_server, port, context=context) as server:
-            # use try and except to handle exceptions
+        def pwd():
+            global password
+            # you need to provide a file that contain your password. 
+            # The file needs to be in th same directory. Or you must specify its path
             try:
-                # ask for the password but masked
-                # you can use this command instead if you 
-                # want to type the password via your terminal without the file
-                # pwd = mk.advpass("Enter your password: ")
+                with open("pass.txt", encoding="utf-8") as f:
+                    for line in f:
+                        password = line.strip()
+                return password
+            except FileNotFoundError:
+                print("You need to provide a file containing your google account app password as pass.txt")
 
-                server.login(sender, pwd())  # new change
-                print("Connected successfully to the SMTP server! 🙂\n")
+        def bulk():
+            global receiver
+            # here you need to provide the file containing the mails you want to send messages to.
+            try:
+                with open("mails.txt", encoding="utf-8") as f:
+                    lines = f.readlines()
+                    for line in lines:
+                        receiver.append(line.strip())
+                    return receiver
+            except FileNotFoundError:
+                print("You need to provide a file containing a list of mails as mails.txt")
 
-                for mail in mails:  # new change
-                    message = f"From: {sender}\nTo: {mail}\n{subject}\n\n" + str(content)  # new change
-                    print("Sending mail to:", mail)  # new change
-                    server.sendmail(sender, mail, message)  # new change
-                print(f"Message sent successfully to {mail} 🙂\n")
-            except SMTPAuthenticationError as e:
-                print("Authentication failed 🙁. Check the error message bellow to see whats going on: \n" + str(e))
-            except Exception as e:
-                print("Something went wrong 🙁. Check the error message bellow to see whats going on:\n" + str(e))
-            except KeyboardInterrupt:
-                answer = input("Do you really want to exit ? yes/no").lower()
-                if answer == "yes":
-                    exit()
-                elif answer == "no":
-                    main()
-            finally:
-                server.quit()
-                print("Now the server is closed!")
+        def rest():
+            global password, mails  # new change
+            # receiver = input("Print the receiver email here: ")
+            content = input("Enter your message here: ")
+            context = ssl.create_default_context()
+            mails = bulk()
+
+            # connect to the smtp server
+            with SMTP_SSL(smtp_server, port, context=context) as server:
+                # use try and except to handle exceptions
+                try:
+                    # ask for the password but masked
+                    # you can use this command instead if you 
+                    # want to type the password via your terminal without the file
+                    # pwd = mk.advpass("Enter your password: ")
+
+                    server.login(sender, pwd())  # new change
+                    print("Connected successfully to the SMTP server! 🙂\n")
+
+                    for mail in mails:  # new change
+                        message = f"From: {sender}\nTo: {mail}\n{subject}\n\n" + str(content)  # new change
+                        print("Sending mail to:", mail)  # new change
+                        server.sendmail(sender, mail, message)  # new change
+                    print(f"Message sent successfully to {mail} 🙂\n")
+                except SMTPAuthenticationError as e:
+                    print("Authentication failed 🙁. Check the error message bellow to see whats going on: \n" + str(e))
+                except Exception as e:
+                    print("Something went wrong 🙁. Check the error message bellow to see whats going on:\n" + str(e))
+                finally:
+                    server.quit()
+                    print("Now the server is closed!")
+
+    except KeyboardInterrupt:
+                    answer = input("Do you really want to exit ? yes/no").lower()
+                    if answer == "yes":
+                        sys.exit()
+                    elif answer == "no":
+                        main()
 
     rest()
 
 
-main()
+    main()
