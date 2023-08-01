f331dda2c79d293f722d940c5034600fa4d3ce5b
Ov3rrlord <desouzajulio16@gmail.com>
2023-08-01 01:20:54 +0100

    Merge pull request #4 from Ov3rrlord/forks
    Merge pull request #3 from Ov3rrlord/main
    
    Signed-off-by: Ov3rrlord <desouzajulio16@gmail.com>

--- bulk_mail.py
+++ bulk_mail.py
@@ -56,7 +56,8 @@ def main():
             # use try and except to handle exceptions
             try:
                 # ask for the password but masked you can use this command instead if you want to type the
-                # password via your terminal without the file pwd = mk.advpass("Enter your password: ")
+                # password via your terminal without the file ù
+                # pwd = mk.advpass("Enter your password: ")
 
                 server.login(sender, pwd())  # new change
                 print(f"Connected successfully to the SMTP server! {chr(0x1F63A)}\n")
